c9ff6139ed29c06290bb3af77a6eaecc93ead32b
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-12-17 11:12:40 +0100

    feat: completed day 2, part 2
    The answer was 36037497037 after some debugging.

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -11,7 +11,7 @@ impl Range {
     }
 
     pub fn iter(&self) -> impl Iterator<Item = u64> {
-        return self.start..self.stop;
+        return self.start..self.stop+1;
     }
 }
 
@@ -80,12 +80,75 @@ pub mod part1 {
     }
 }
 
+pub mod part2 {
+    use super::{Range, parse};
+
+    /// Checks if an ID is valid.
+    pub fn is_invalid_id(id: u64) -> bool {
+        let str = id.to_string();
+        let possible_substring_lengths = 1..(str.len()/2)+1;
+        let numbers: Vec<u64> = str.chars().map(|c| c.to_string().parse().unwrap()).collect();
+
+        println!("\tpossible_substring_lengths: {possible_substring_lengths:?} in {str}");
+        for length in possible_substring_lengths {
+            if str.len() % length != 0 { continue; }
+
+            let mut found = true;
+            let substring1 = numbers.get(0..length).unwrap();
+            println!("\t\tlength: {length}, substring1: {substring1:?}");
+            for index in 1..(numbers.len()/length) {
+                let next_slice_indexes = index*length..(1+index)*length;
+                println!("\t\t\tLooking for numbers in range: {next_slice_indexes:?}");
+                let substring2 = numbers.get(next_slice_indexes).unwrap();
+                println!("\t\t\tcomparing '{substring1:?}' with '{substring2:?}' in {str}");
+                if substring2 != substring1 {
+                    found = false;
+                    println!("\t\t\t\tThey are not the same");
+                    break;
+                }
+            }
+
+            if found {
+                println!("\tfound! in {str} with length {length}");
+                return true;
+            }
+        }
+
+        println!("\tnot found!");
+        return false;
+    }
+
+    pub fn compute(input: Vec<Range>) -> u64 {
+        let mut count = 0;
+
+        for range in input {
+            println!("Looking through {range:?} for invalid IDs:");
+            for number in range.iter() {
+                if is_invalid_id(number) {
+                    println!("\tFound {number} in {range:?} total: {count}");
+                    count += number;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /// Returns the computation from the input
+    pub fn solve(input: &str) -> u64 {
+        return compute(parse(input));
+    }
+}
+
 fn main() {
     const INPUT: &str = include_str!("../input/day2.txt");
     let input = parse(INPUT);
 
-    let result1 = part1::compute(input);
+    let result1 = part1::compute(input.clone());
     println!("The solution to part 1 is {result1}");
+
+    let result2 = part2::compute(input);
+    println!("The solution to part 2 is {result2}");
 }
 
 #[cfg(test)]
@@ -98,6 +161,13 @@ mod tests {
     fn test_part1() {
         let result = part1::solve(INPUT);
         let expected = 1227775554;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2::solve(INPUT);
+        let expected = 4174379265;
         assert_eq!(result, expected);
     }
 }
